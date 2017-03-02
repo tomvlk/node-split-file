66a2e315d2741e7d288b6ce73ab791fc3ea1dec8
Tom Valk <tomvalk@gmail.com>
2017-03-02 21:23:54 +0100

    [TASK] Refactor CLI methods.

--- split-file-cli.js
+++ split-file-cli.js
@@ -2,30 +2,55 @@
 
 var split = require('./split-file.js');
 
-if (require.main === module) {
-  cli();
-}
+var Cli = function () {};
 
-function cli() {
-  var option = process.argv[2];
+/**
+ * Parse cli option.
+ */
+Cli.prototype.parse = function (option) {
+  this.option = option;
+
   switch (option) {
     case '-m':
-      cliMerge();
+      this.method = this.merge;
       break;
     case '-s':
-      cliSplit();
+      this.method = this.split;
       break;
     default:
-      printLegend();
+      this.method = this.help;
   }
+
+  return this;
 }
 
-function cliSplit() {
+/**
+ * Print the legend.
+ */
+Cli.prototype.help = function () {
+  console.log("Usage: split-file -s input.bin 5");
+  console.log("       split-file -m output.bin part1 part2 ...");
+  console.log("");
+  console.log(" -s <input> <num_parts>");
+  console.log("    Split the input file in the number of parts given.");
+  console.log("");
+  console.log(" -m <output> <part> <part> ...");
+  console.log("    Merge the given parts into the output file.");
+  console.log("");
+  console.log("");
+  console.log("NPM Module 'split-file' by Tom Valk.");
+  console.log("Visit https://github.com/tomvlk/node-split-file for info and help.");
+}
+
+/**
+ * Split command.
+ */
+Cli.prototype.split = function () {
   var file = process.argv[3];
   var parts = parseInt(process.argv[4]);
   
   if (isNaN(parts)) {
-    return printLegend();
+    return this.help();
   }
 
   split.splitFile(file, parts).then(function (names) {
@@ -36,7 +61,10 @@ function cliSplit() {
   });
 }
 
-function cliMerge() {
+/**
+ * Merge command.
+ */
+Cli.prototype.merge = function () {
   var files = [];
   var output_file = process.argv[3];
 
@@ -52,17 +80,13 @@ function cliMerge() {
   });
 }
 
-function printLegend() {
-  console.log("Usage: split-file -s input.bin 5");
-  console.log("       split-file -m output.bin part1 part2 ...");
-  console.log("");
-  console.log(" -s <input> <num_parts>");
-  console.log("    Split the input file in the number of parts given.");
-  console.log("");
-  console.log(" -m <output> <part> <part> ...");
-  console.log("    Merge the given parts into the output file.");
-  console.log("");
-  console.log("");
-  console.log("NPM Module 'split-file' by Tom Valk.");
-  console.log("Visit https://github.com/tomvlk/node-split-file for info and help.");
+Cli.prototype.run = function () {
+  return this.method();
+}
+
+if (require.main === module) {
+  var cli = new Cli();
+  cli
+    .parse(process.argv[2])
+    .run();
 }
