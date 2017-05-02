dd22bd1c340b095c1a1646e494f2107fa94c42c5
Tom Valk <tomvalk@gmail.com>
2017-05-02 20:09:16 +0200

    [TASK] Add more tests for the splitting of files based on number of parts.

--- __tests__/split-file.js
+++ __tests__/split-file.js
@@ -25,6 +25,7 @@ function checksumFile(file, algorithm, encoding) {
 }
 
 const md5Zip = '561a3c354bbca14cf501d5e252383387';
+const md5Pdf = '6bb492c383240fcd87b5c42958c2e482';
 
 
 describe('split and merge on size', () => {
@@ -71,6 +72,64 @@ describe('split and merge on size', () => {
 
                 expect(mergedStat.size).toBe(originalStat.size);
                 expect(md5Zip).toBe(checksumFile(output));
+                done();
+            }).catch((err) => {
+                console.error(err);
+                expect(err).toBeNull();
+                done();
+            });
+    });
+
+
+    afterAll(() => {
+        cleanUp();
+    });
+});
+
+
+
+describe('split and merge on number of parts', () => {
+    test('should create the parts based on number of given parts', (done) => {
+        const input = __dirname + '/files/test2/sample.pdf';
+        const inputStat = fs.statSync(input);
+        const numberOfParts = 512;
+
+        return splitFile.splitFile(input, numberOfParts).then((parts) => {
+            let totalPartsSize = 0;
+            parts.forEach((part) => {
+                let stat = fs.statSync(part);
+                totalPartsSize += stat.size;
+            });
+            expect(totalPartsSize).toBe(inputStat.size);
+            done();
+    	}).catch((err) => {
+            console.error(err);
+            expect(err).toBeNull();
+            done();
+        });
+    });
+
+    test('should merge the splitted files', (done) => {
+        let files = [];
+
+        const base = __dirname + '/files/test2/sample.pdf.sf-part';
+        const output = __dirname + '/files/test2/sample.out';
+        const input = __dirname + '/files/test2/sample.pdf'
+
+        const dir = fs.readdirSync(testRoot + '/files/test2/');
+        dir.forEach((file) => {
+            if (file.indexOf('sf-part') != -1) {
+                files.push(testRoot + '/files/test2/' + file);
+            }
+        })
+
+        splitFile.mergeFiles(files, output)
+            .then(() => {
+                const originalStat = fs.statSync(input);
+                const mergedStat = fs.statSync(output);
+
+                expect(mergedStat.size).toBe(originalStat.size);
+                expect(md5Pdf).toBe(checksumFile(output));
                 done();
             }).catch((err) => {
                 console.error(err);
