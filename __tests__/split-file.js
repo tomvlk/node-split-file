5741184cb83a11031b05ef940c8dee258d5683c7
Tom Valk <tomvalk@gmail.com>
2017-05-02 17:23:06 +0200

    [TASK] Update tests.

--- __tests__/split-file.js
+++ __tests__/split-file.js
@@ -5,17 +5,29 @@ const fs = require('fs');
 const testRoot = __dirname;
 
 describe('split', () => {
-    it('should create 5 zip parts', (done) => {
+    it('should create 6 parts based on bytes of split parts', (done) => {
+        const input = __dirname + '/files/sample.zip';
+        const splitSize = 100000;
+
+        splitFile.splitFileBySize(input, splitSize).then((parts) => {
+            if (parts.length === 6) {
+                return done();
+            }
+            throw new Error('Wrong number of splitted parts! Got ' + parts.length);
+    	});
+    });
+
+    it('should create 6 zip parts', (done) => {
         const input = __dirname + '/files/sample.zip';
 
-        splitFile.splitFile(input, 5)
+        splitFile.splitFile(input, 6)
             .then((parts) => {
                 if (parts) {
                     return done();
                 }
-                return done(new Error('Parts not defined or filled!'));
+                throw new Error('Parts not defined or filled!');
             }).catch((err) => {
-                return done(err);
+                throw err;
             });
     });
 });
@@ -23,16 +35,16 @@ describe('split', () => {
 describe('merge', () => {
     it('should merge the splitted files', (done) => {
         let files = [];
-        
+
         const base = __dirname + '/files/sample.zip.sf-part';
         const output = __dirname + '/files/sample.out';
         const input = __dirname + '/files/sample.zip'
 
-        for (var i = 0; i < 3; i++) {
-            files.push(base + (i+1));
+        for (var i = 1; i <= 6; i++) {
+            files.push(base + i);
         }
 
-        splitFile.mergeFiles([files], output)
+        splitFile.mergeFiles(files, output)
             .then(() => {
                 const originalStat = fs.statSync(input);
                 const mergedStat = fs.statSync(output);
@@ -40,9 +52,9 @@ describe('merge', () => {
                 if (originalStat.size === mergedStat.size) {
                     return done();
                 }
-                return done(new Error('Input and output doesn\'t equal in size'));
+                throw new Error('Input and output doesn\'t equal in size');
             }).catch((err) => {
-                return done(err);
+                throw err;
             });
     });
 });
